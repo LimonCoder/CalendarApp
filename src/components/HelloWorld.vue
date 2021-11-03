1a24018ad67d0cd5d9ebbb28afd6348dcb3a693f
Nurul Amin Limon <nurulaminlimon@gmail.com>
2021-11-04 01:01:04 +0600

    init commit calandar app using vue.js

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,72 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <section>
+    <h2 class="text-danger text-center">My calander vue</h2>
+  </section>
+  <section class="bg-primary d-flex flex-column justify-content-center"
+  ><h1 class="text-black">{{ getCurrentMonthName }}, {{ currentYear }}</h1></section>
+  <section>
+    <div class="days d-flex flex-row">
+      <br>
+      <p class="text-center " v-for="(day,key) in days" :key="key"><b>{{ day }}</b></p>
+    </div>
+    <div class="dates d-flex flex-row">
+      <p class="text-center" v-for="(date,key) in getFastDayInMonth" :key="key"></p>
+      <p :class="todayDate(date) ? 'text-primary':'' " v-for="(date,key) in getLastDayInMonth" :key="key">{{ date }}</p>
+    </div>
+  </section>
+  <br>
+  <br>
+  <br>
+  <section class="d-flex justify-content-around">
+    <button class="btn btn-primary" @click="prev()">prev</button>
+    <button class="btn btn-primary" @click="next()">next</button>
+  </section>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data: function () {
+    return {
+      days: ['Sun', 'Mon', 'Tu', 'Wed', 'Thus', 'Fri', 'Sat'],
+      currentYear: (new Date()).getFullYear(),
+      currentMonth: (new Date()).getMonth(),
+      currentDay: (new Date()).getDate(),
+    }
+  },
+  computed: {
+    getCurrentMonthName() {
+      return new Date(this.currentYear, this.currentMonth, this.currentDay).toLocaleString('default', {month: 'long'});
+    },
+    getLastDayInMonth() {
+      return new Date(this.currentYear, this.currentMonth + 1, 0).getDate();
+    },
+    getFastDayInMonth() {
+      return new Date(this.currentYear, this.currentMonth, 1).getDay();
+    }
+  },
+  methods: {
+    prev() {
+      if (this.currentMonth == 0) {
+        this.currentMonth = 11;
+        this.currentYear--;
+      } else {
+        this.currentMonth--;
+      }
+    },
+    next() {
+      if (this.currentMonth == 11) {
+        this.currentMonth = 0;
+        this.currentYear++;
+      } else {
+        this.currentMonth++;
+      }
+    },
+    todayDate(date){
+      console.log(new Date(this.currentYear, this.currentMonth, date).toDateString());
+      return  new Date(this.currentYear, this.currentMonth, date).toDateString() == new Date().toDateString();
+    }
   }
 }
 </script>
@@ -44,15 +76,26 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
+}
+
+.days p, .dates p {
+  width: 14.28%;
+}
+
+.dates {
+  flex-wrap: wrap;
 }
 </style>
